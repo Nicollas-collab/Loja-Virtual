c0eea54c345a3dd53c44fc4d1fc0ea97016bd2ea
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-17 21:31:23 -0300

    cadastro não finalizado

--- js/consulta_cep.js
+++ js/consulta_cep.js
@@ -3,10 +3,10 @@ const inputCep = document.querySelector('#cep')
 
 //CAPTURANDO O EVENTO AO PERDER O FOCO
 inputCep.addEventListener('change', (evt)=>{
-    //PEGANDO
-    const numCep = evt.target.value.replace(/\D/g,'')
+    //PEGANDO OS NÚMEROS DO INPUT NÃO PERMITINDO OUTRO TIPO DE DADOS QUE NÃO SEJA DIGITO
+    const numCep = evt.target.value.replace(/\D/g,"")
 
-    if(numCep != 8){
+    if(numCep !== 8){
         alert('CEP INVÁLIDO')
         return
     }
@@ -17,6 +17,8 @@ inputCep.addEventListener('change', (evt)=>{
 //BUSCAR OS DADOS DOS CEP NO VIACEP
 
 const buscaDadosCep = async (cep) => {
+    //comando ASYNC: se pede algo que precisa aguarda, ele permite que o comando continue rodando e não interrompa o sistema.
+
     //TENTA BUSCAR OS DADOS NO VIACEP
 
     try{
@@ -29,7 +31,8 @@ const buscaDadosCep = async (cep) => {
         exibeDados(dados)
     })
 
-
+    //comando fetch: busca algum dado que está fora do sistema java
+    //comando await: faz esperar o fetch trazer algo
         const response = await fetch('https://viacep.com.br/ws/01001000/json/')
         /*.then(resp => resp.json())
         .then((dadosEndereco) =>{
@@ -37,7 +40,7 @@ const buscaDadosCep = async (cep) => {
         })*/
 
     //CONVERTE OS DADOS NO FORMATO json
-        const dadosEndereco = response.json()
+        const dadosEndereco = await response.json()
         console.log(dadosEndereco)
 
     //CHAMA A FUNÇÃO exibeDados
@@ -49,28 +52,37 @@ const buscaDadosCep = async (cep) => {
     }
 }
 
-//OBJETO LITERAL CAMPOS QUE CRIA REPRESENTA OS INPUT DO DOM
+//OBJETO LITERAL CAMPOS QUE CADA CHAVE REPRESENTA OS INPUT DO DOM
 const campos = {
 
-    logradouro: document.querySelector('#logradouro'),
-    bairro: document.querySelector('#bairro'),
-    localidade: document.querySelector('#localidade'),
-    uf: document.querySelector("#uf"),
+    logradouro: document.querySelector('#logradouro').value = objDados.logradouro,
+    bairro: document.querySelector('#bairro').value = objDados.bairro,
+    localidade: document.querySelector('#localidade').value = objDados.localidade,
+    uf: document.querySelector("#uf").value = objDados.uf
 
 }
 
 //FUNÇÃO EXIBE DADOS
-const exibeDados = (objDados) =>{
-
+const exibeDados = (objDados) => {
     //PEGA A DIV PAI DOS ELEMENTOS DO ENDEREÇO
     const divEndereco = document.querySelector('#div-dados-endereco')
 
     //REMOVE DA DIV O CLASS OCULTO
     divEndereco.classList.remove('oculto')
 
-    //PERCORRE OBJETO, NO FORMATO JSON, DO VIDA CEP
-    for(let chave in objDados) {
-    //ATRIBUI O VALOR AO INPUT
-        campos[chave].value
+    //PERCORRE O OBJETO, NO FORMATO JSON, DO VIDA CEP
+    for (let chave in campus) {
+        //ATRIBUI O VALOR AO INPUT
+        campos[chave].value = objDados[chave]
+
+        //BLOQUEIA OS INPUTS. NÃO PERMITE QUE O USUÁRIO APAGUE OS VALORES
+        campos[chave].disable = objDados[chave]
     }
+    document.querySelector
+
 }
+formPessoa.addEventListener('reset',() =>{
+    //PEGA A DIV PAI DOS ELEMENTOS DO ENDEREÇO
+    const divEndereco = document.querySelector('#div-dados-endereco')
+    
+})
